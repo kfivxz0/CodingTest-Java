a0bb32f15a2f892772dfe06dc58f88f78e9f6bd0
LeeMinkyu <kfivxz0@gmail.com>
2024-03-20 22:36:57 +0900

    3월20일 추가 문제풀이

---
+++ Programmers/BasicTraining/Lv.0/24.03.20/n의 배수.java
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181937
+
+// 문제 설명
+// 정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록
+// solution 함수를 완성해주세요.
+
+class Solution {
+  public int solution(int num, int n) {
+    if (num % n == 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
