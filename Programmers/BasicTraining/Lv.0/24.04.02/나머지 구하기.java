89ce7337fd3bf2229dda916dd4a5bee66105cfea
LeeMinkyu <kfivxz0@gmail.com>
2024-04-02 15:52:26 +0900

    4월2일 문제풀이

---
+++ Programmers/BasicTraining/Lv.0/24.04.02/나머지 구하기.java
@@ -0,0 +1,11 @@
+// https://kfivxz0.tistory.com/entry/%EC%84%B8%ED%8C%85
+
+// 문제 설명
+// 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+
+// 풀이
+class Solution {
+  public int solution(int num1, int num2) {
+    return num1 % num2;
+  }
+}
