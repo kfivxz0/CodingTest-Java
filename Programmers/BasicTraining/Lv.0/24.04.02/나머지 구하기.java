22f12f7d416e83fbf6c635c1bc2a56b6f3a40acb
LeeMinkyu <kfivxz0@gmail.com>
2024-04-03 14:29:18 +0900

    4월3일 문제풀이

--- Programmers/BasicTraining/Lv.0/24.04.02/나머지 구하기.java
+++ Programmers/BasicTraining/Lv.0/24.04.02/나머지 구하기.java
@@ -8,4 +8,4 @@ class Solution {
   public int solution(int num1, int num2) {
     return num1 % num2;
   }
-}
+} 
--- Test.java
+++ Test.java
@@ -1,22 +1,7 @@
+public class Test {
+  int a = 0;
 
-// class Solution {
-// public int[] solution(int[] num_list) {
-// int[] answer = { num_list.length + 1 };
-// int n = 0;
-
-// if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
-// n = num_list[num_list.length - 1] - num_list[num_list.length - 2];
-
-// } else {
-// n = num_list[num_list.length - 1] * 2;
-
-// }
-
-// for (int i = 0; i < answer.length; i++) {
-// answer[i] = num_list[i];
-// }
-// answer[answer.length - 1] = n;
-// return answer;
-// }
-
-// }
+  public static void main(String[] args) {
+    int b = 0;
+  }
+}
