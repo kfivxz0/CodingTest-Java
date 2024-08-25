f686f8956ca34b5770e12ff3fdc36fd7cd55242b
kfivxz0 <kfivxz0@gmail.com>
2024-08-25 13:07:00 +0900

    8월25일 문제풀이

---
+++ Programmers/BasicTraining/Lv.0/24.08.25/대문자로 바꾸기.java
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181877
+
+// 문제 설명
+// 알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 대문자로 변환하여 return 하는 solution 함수를 완성해
+// 주세요.
+
+// 풀이
+class Solution {
+  public String solution(String myString) {
+    return myString.toUpperCase();
+  }
+}
