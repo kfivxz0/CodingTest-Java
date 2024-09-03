498dab1a3e651667206aa31c5e94298deae66eb7
kfivxz0 <kfivxz0@gmail.com>
2024-09-03 17:47:33 +0900

    9월3일 문제풀이

--- Programmers/BasicTraining/Lv.0/24.09.03/조건에 맞게 수열 반환하기1.java
+++ Programmers/BasicTraining/Lv.0/24.09.03/조건에 맞게 수열 반환하기1.java
@@ -6,13 +6,13 @@
 // 풀이
 class Solution {
   public int[] solution(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i] >= 50 && arr[i] % 2 == 0) {
-        arr[i] /= 2;
-      } else if (arr[i] < 50 && arr[i] % 2 == 1) {
-        arr[i] *= 2;
+      for(int i=0; i<arr.length; i++){
+        if(arr[i] >= 50 && arr[i] % 2 == 0){
+          arr[i] /= 2;
+        } else if(arr[i] < 50 && arr[i] % 2 == 1) {
+          arr[i] *= 2;
+        }
       }
-    }
-    return arr;
+      return arr;
   }
 }
