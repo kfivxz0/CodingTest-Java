a415b5639976ef1671b1ac3f24203cd0ec5839b9
kfivxz0 <kfivxz0@gmail.com>
2024-09-11 14:54:37 +0900

    9월11일 문제풀이

---
+++ Programmers/BasicTraining/Lv.1/24.09.11/두 정수 사이의 합.java
@@ -0,0 +1,26 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12912
+
+// 문제 설명
+// 두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+// 예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+
+//풀이
+class Solution {
+  public long solution(int a, int b) {
+    long answer = 0;
+
+    if (a < b) {
+      for (int i = a; i <= b; i++) {
+        answer += i;
+      }
+    } else if (a > b) {
+      for (int i = b; i <= a; i++) {
+        answer += i;
+      }
+    } else {
+      answer = a;
+    }
+
+    return answer;
+  }
+}
