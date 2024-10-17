2ad50131f4b75d6a4004817246190d9f07986d70
kfivxz0 <kfivxz0@gmail.com>
2024-10-17 12:47:33 +0900

    10월17일 문제풀이

---
+++ Programmers/MySQL/Lv.1/24.10.17/상위 n개 레코드.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59405
+
+-- 풀이
+SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1
---
+++ Programmers/MySQL/Lv.1/24.10.17/여러 기준으로 정렬하기.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59404
+
+-- 풀이
+SELECT ANIMAL_ID, NAME, DATETIME 
+FROM ANIMAL_INS
+ORDER BY NAME ASC, 
+DATETIME DESC;
