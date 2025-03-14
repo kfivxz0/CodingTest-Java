f4f9a00e40c2f90e84f5fb7aa2e1c0e4709b1594
kfivxz0 <kfivxz0@gmail.com>
2025-03-14 20:51:02 +0900

    3월14일  문제풀이

---
+++ Programmers/MySQL/Lv.2/25.03.14/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -0,0 +1,16 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131115
+
+-- 문제 설명
+-- 다음은 식품의 정보를 담은 FOOD_PRODUCT 테이블입니다. FOOD_PRODUCT 테이블은 다음과 같으며 PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE는 식품 ID, 식품 이름, 식품 코드, 식품분류, 식품 가격을 의미합니다.
+
+-- Column name	Type	Nullable
+-- PRODUCT_ID	VARCHAR(10)	FALSE
+-- PRODUCT_NAME	VARCHAR(50)	FALSE
+-- PRODUCT_CD	VARCHAR(10)	TRUE
+-- CATEGORY	VARCHAR(10)	TRUE
+-- PRICE	NUMBER	TRUE
+
+-- 풀이
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+FROM FOOD_PRODUCT
+WHERE PRICE = (SELECT MAX(PRICE) FROM FOOD_PRODUCT);
